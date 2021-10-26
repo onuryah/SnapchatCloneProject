e94b5339e6f232c63ceba2b0549ca46a3bf58ce0
Onuralp <xonuraralpx@gmail.com>
2021-10-26 23:42:54 +0300

    Initial Commit

---
+++ SnapchatProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SnapchatProject
+//
+//  Created by Ceren Çapar on 26.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
